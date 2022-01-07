4836c58469b8877500933625189a2a0fe4ebcd1d
may-astr.java <vsirjdcrfz@mail.ru>
2022-01-07 20:04:24 +0400

    урок 6, ничего не понимаю,- разбираюсь

--- com.geekbrains/src/main/java/com/geekbrais/oop/Animal.java
+++ com.geekbrains/src/main/java/com/geekbrais/oop/Animal.java
@@ -9,6 +9,7 @@ public class Animal {
     public  Animal(){}
 
     public Animal(String name, String color, int age, String masterNameint, int run, int sail) {
+        
         this.name = name;
         this.color = color;
         this.age= age;
--- com.geekbrains/src/main/java/com/geekbrais/oop/Cat.java
+++ com.geekbrains/src/main/java/com/geekbrais/oop/Cat.java
@@ -1,6 +1,7 @@
 package com.geekbrais.oop;
     public class Cat extends Animal{
         public Cat(String name, String color, int age, String masterName, int run, int sail){
+
             super(name, color, age, masterName, run, sail);
     }
 }
