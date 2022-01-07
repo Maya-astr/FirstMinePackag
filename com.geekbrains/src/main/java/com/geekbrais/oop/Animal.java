5cfb30c1f5c38a7bdb1169eeb3a691d3f091544d
may-astr.java <vsirjdcrfz@mail.ru>
2022-01-07 19:43:56 +0400

    урок 6, ничего не понимаю

---
+++ com.geekbrains/src/main/java/com/geekbrais/oop/Animal.java
@@ -0,0 +1,31 @@
+package com.geekbrais.oop;
+public class Animal {
+    protected String name;
+    protected String color;
+    protected int age;
+    protected String masterName;
+    protected int run;
+    protected int sail;
+    public  Animal(){}
+
+    public Animal(String name, String color, int age, String masterNameint, int run, int sail) {
+        this.name = name;
+        this.color = color;
+        this.age= age;
+        this.masterName = masterNameint;
+        this.run = run;
+        this.sail = sail;
+    }
+    public void Dog() {
+        System.out.println("Собака " + name + "   бежит  " + run + "   метров");
+        System.out.println("Собака " + name + "   плывет  " + sail + "   метров");
+    }
+
+}
+
+
+
+
+
+
+
---
+++ com.geekbrains/src/main/java/com/geekbrais/oop/Cat.java
@@ -0,0 +1,10 @@
+package com.geekbrais.oop;
+    public class Cat extends Animal{
+        public Cat(String name, String color, int age, String masterName, int run, int sail){
+            super(name, color, age, masterName, run, sail);
+    }
+}
+
+
+
+
